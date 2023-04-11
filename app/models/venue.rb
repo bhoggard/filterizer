cb230244ae67f9c75be2698ecb2251aa7c8d9190
Barry Hoggard <barry@hoggard.org>
2023-04-10 20:25:16 -0400

    set default sort on venue

--- app/models/venue.rb
+++ app/models/venue.rb
@@ -3,4 +3,6 @@ class Venue < ApplicationRecord
   has_many :events
   validates :name, :address, :neighborhood_id, presence: true
   validates :website, format: {with: URI::DEFAULT_PARSER.regexp[:ABS_URI]}
+
+  default_scope { order(:name) }
 end
